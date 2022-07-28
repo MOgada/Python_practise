4fe5fc2bc8ea9150bac4c00625d6386ca252e123
MOgada <40202873+MOgada@users.noreply.github.com>
2022-07-29 02:57:03 +0300

    Add files via upload

---
+++ List_Values_same_or_not.py
@@ -0,0 +1,18 @@
+y = [10,20,30,40,10]
+x = [20,30,40,50,60]
+i = 0
+
+if y[0] == y[4]:
+    print ("Given List:", y)
+    print("The value is the same")
+elif y[0] != y[4]:
+    print("The value is not the same")
+
+if x[0] == x[4]:
+    print ("Given List:", x)
+    print("The value is the same")
+elif x[0] != x[4]:
+    print("The value is not the same")
+
+
+
